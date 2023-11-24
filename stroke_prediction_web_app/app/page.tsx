0cfd3d5dd22df0f7b64f2cc2872d69574a474d8f
Kalva014 <kalva014@ucr.edu>
2023-11-24 11:36:06 -0800

    Can route between pages

--- stroke_prediction_web_app/app/page.tsx
+++ stroke_prediction_web_app/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
       <h1>See How Susceptible You Are To Stroke</h1>
+      <h2>
+        <a href="posts/">Check your percentage</a>
+      </h2>
     </main>
   )
 }
